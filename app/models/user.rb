9c2488b8dda46bf4cbafab40f94f255bf9000796
Philipe <dev.philipe@outlook.com>
2023-07-04 22:45:24 -0300

    feat(user): user is saving on database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  has_secure_password
+  before_save :email_downcase
+
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :password, length: { minimum: 6 }
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: URI::MailTo::EMAIL_REGEXP },
+            uniqueness: { case_sensitive: false }
+  private
+  def email_downcase
+    self.email.downcase!
+  end
 end
