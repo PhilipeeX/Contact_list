4a65bb0c634706886acde36525e58e973e8400dd
Philipe <dev.philipe@outlook.com>
2023-07-08 11:59:03 -0300

    fix(Contact): colon

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, length: { maximum: 50 }
-  validates :phone, presence: true,
+  validates :phone, presence: true
 end
